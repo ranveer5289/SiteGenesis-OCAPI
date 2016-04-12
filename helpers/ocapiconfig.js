a7b323c87333b14256621a2a9797f5a0c78fd2f7
ranveer raghuwanshi <ranveer.raghuwanshi@osf-global.com>
2016-04-12 19:24:55 +0530

    Deleted file

--- helpers/ocapiconfig.js
+++
@@ -1,9 +0,0 @@
-var config = {};
-
-config.httphost = "http://dev01.latam.loreal.demandware.net";
-config.httpshost = "https://dev01.latam.loreal.demandware.net";
-config.siteid = "SiteGenesis";
-config.ocapiversion = "16_3";
-config.clientid = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-
-module.exports = config;
