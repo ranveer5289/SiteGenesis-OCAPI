3ed88e64b7ad15aec99478d3214e423022734c29
ranveer raghuwanshi <ranveer.raghuwanshi@osf-global.com>
2016-04-16 19:52:40 +0530

    Added sample file

---
+++ ocapiconfig.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.httphost = "http://xxx.yyy.xxx.demandware.net";
+config.httpshost = "https://xxx.yyy.xxx.demandware.net";
+config.siteid = "SiteGenesis";
+config.ocapiversion = "16_3";
+config.clientid = "your_client_id";
+
+config.paymentmethodid = "CREDIT_CARD";
+config.defaultshippingmethodid = "001";
+
+module.exports = config;
