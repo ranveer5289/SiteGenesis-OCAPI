325c4dbf0f6c1b44bf0fd2b00c26e78e92be38ca
Vandna Singla <vandna.singla@osf-global.com>
2016-04-19 16:53:20 +0530

    updated config

--- ocapiconfig.js
+++ ocapiconfig.js
@@ -1,10 +1,10 @@
 var config = {};
 
-config.httphost = "http://xxx.yyy.xxx.demandware.net";
-config.httpshost = "https://xxx.yyy.xxx.demandware.net";
+config.httphost = "http://dev01.latam.loreal.demandware.net";
+config.httpshost = "https://dev01.latam.loreal.demandware.net";
 config.siteid = "SiteGenesis";
 config.ocapiversion = "16_3";
-config.clientid = "your_client_id";
+config.clientid = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 config.paymentmethodid = "CREDIT_CARD";
 config.defaultshippingmethodid = "001";
