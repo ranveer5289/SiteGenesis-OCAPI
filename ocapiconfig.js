b4082058e0e8765946f62cf27b74825fb0107560
Ranveer Raghuwanshi <ranveer.raghu@gmail.com>
2016-04-19 17:08:02 +0530

    Update ocapiconfig.js

--- ocapiconfig.js
+++ ocapiconfig.js
@@ -1,10 +1,10 @@
 var config = {};
 
-config.httphost = "http://dev01.latam.loreal.demandware.net";
-config.httpshost = "https://dev01.latam.loreal.demandware.net";
+config.httphost = "http://xxx.yyy.zzz.demandware.net";
+config.httpshost = "https://xxx.yyy.zzz.demandware.net";
 config.siteid = "SiteGenesis";
 config.ocapiversion = "16_3";
-config.clientid = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+config.clientid = "your_client_id";
 
 config.paymentmethodid = "CREDIT_CARD";
 config.defaultshippingmethodid = "001";
